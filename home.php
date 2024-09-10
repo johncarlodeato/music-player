75e23dcf8e450e8da01f54e85cd2b27834f20d7e
john carlo deato <deato.johncarlo.antalan@gmail.com>
2024-09-10 15:46:33 +0800

    new files

--- home.php
+++ home.php
@@ -16,7 +16,7 @@ include 'connect.php';
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css">
 
    <!-- custom css file link  -->
-   <link rel="stylesheet" href="css/style.css">
+   <link rel="stylesheet" href="style.css">
 
 </head>
 <body>
